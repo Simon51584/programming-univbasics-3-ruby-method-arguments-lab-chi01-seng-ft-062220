e9acae178b6aba53243dc7659b1a109130f80b24
Seth Starr <starrseth@hotmail.com>
2020-06-04 20:40:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,9 +2,13 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+introduction(name)
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learnign to program in #{language}."
 end 
+
+introduction_with_language()
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, ny name is #{name} and I am learning to program in #{language}."
